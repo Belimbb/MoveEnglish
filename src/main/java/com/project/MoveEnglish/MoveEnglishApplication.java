41ae7551d0ea9c69a5812d86fd654e91e6e83df3
Belimbb <ol3ksii.bielov@gmail.com>
2025-06-16 21:39:49 +0300

    Initial commit

---
+++ src/main/java/com/project/MoveEnglish/MoveEnglishApplication.java
@@ -0,0 +1,13 @@
+package com.project.MoveEnglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveEnglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveEnglishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/MoveEnglish/MoveEnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MoveEnglish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveEnglishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
