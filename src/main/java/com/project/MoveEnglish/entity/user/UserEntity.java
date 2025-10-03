028ad6ea017a7f72410701c5fe7dfdc50073fb7b
Belimbb <ol3ksii.bielov@gmail.com>
2025-10-03 20:00:52 +0200

    Hotfix 2 Render patch. Works on Docker Desktop

--- src/main/java/com/project/MoveEnglish/entity/user/UserEntity.java
+++ src/main/java/com/project/MoveEnglish/entity/user/UserEntity.java
@@ -1,11 +1,9 @@
 package com.project.MoveEnglish.entity.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -18,4 +16,36 @@ public class UserEntity {
 
     @Enumerated(EnumType.STRING)
     private UserState state;
+
+    public UserState getState() {
+        return state;
+    }
+
+    public void setState(UserState state) {
+        this.state = state;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
--- src/main/java/com/project/MoveEnglish/entity/user/UserMapper.java
+++ src/main/java/com/project/MoveEnglish/entity/user/UserMapper.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface UserMapper {
     UserEntity toEntity(UserDto request);
 
-    @Mapping(target = "state", source = "state")
+    //@Mapping(target = "state", ignore = false)
     UserDto toDto(UserEntity userEntity);
 
     List<UserDto> toDtoList(List<UserEntity> entities);
